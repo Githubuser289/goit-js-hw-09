9ed5f9b28e9c2f825c91f659c840db479d988ac9
Githubuser289 <moldovan.mms@gmail.com>
2023-11-11 23:28:49 +0200

    updated ex1 stopBtn

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -11,15 +11,18 @@ function activateChangeColor() {
     bodyElem.style.backgroundColor = getRandomHexColor();
   }, 1000);
   startBtn.disabled = true;
+  stopBtn.disabled = false;
 }
 function deactivateChangeColor() {
   clearInterval(timerId);
   startBtn.disabled = false;
+  stopBtn.disabled = true;
 }
 
 const bodyElem = document.getElementsByTagName('body')[0];
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
+stopBtn.disabled = true;
 
 // atasez eventlistener pe cele 2 butoane
 // la apasarea unuia, il dezactivez pe celalalt
